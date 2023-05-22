fdc0fda8a230d632e22b0ba88be4ab810b2b0e26
James <jamesbaxterse@live.co.uk>
2023-05-22 21:52:05 +0100

    Some updates. It's *nearly* working...

--- perforce-server/docker-startup.d/02_configure_P4D.sh
+++ perforce-server/docker-startup.d/02_configure_P4D.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 set -e
 
-# Configure service if it hasn't been done already
-if ! perforce p4dctl list 2>/dev/null | grep -q "${P4SVCNAME}"; then
+# If already configured, nothing to do but start the service
+if p4dctl list 2>/dev/null | grep -q "${P4SVCNAME}"; then
 
-	# Assemble the command string to configure perforce in non-interactive mode
-	echo "Configuring P4..."
-	CONFIGURE_P4D_CMD=("/opt/perforce/sbin/configure-helix-p4d.sh")
-	CONFIGURE_P4D_CMD+=("${P4SVCNAME}")
-	CONFIGURE_P4D_CMD+=("-n")
-	CONFIGURE_P4D_CMD+=("-p" "${P4PORT}")
-	CONFIGURE_P4D_CMD+=("-r" "${P4ROOT}")
-	CONFIGURE_P4D_CMD+=("-u" "${P4USER}")
-	CONFIGURE_P4D_CMD+=("-P" "${P4PASSWD}")
+	echo "Starting existing service '${P4SVCNAME}'"
 	
-	if [[ "${P4D_CASE_SENSITIVE}" == "true" ]]; then
-		CONFIGURE_P4D_CMD+=("--case" "0")
-	else
-		CONFIGURE_P4D_CMD+=("--case" "1")
-	fi
+	# Start the Service #TODO if not already started
+	p4dctl start -t p4d "${P4SVCNAME}"
+	
+	exit 0
+fi
+	
+# Assemble the command string to configure perforce in non-interactive mode
+echo "Configuring new p4dctl service '${P4SVCNAME}'"
 
-	"${CONFIGURE_P4D_CMD[@]}" 1>/dev/null
+CONFIGURE_P4D_CMD=("/opt/perforce/sbin/configure-helix-p4d.sh")
+CONFIGURE_P4D_CMD+=("${P4SVCNAME}")
+CONFIGURE_P4D_CMD+=("-n")
+CONFIGURE_P4D_CMD+=("-p" "${P4PORT}")
+CONFIGURE_P4D_CMD+=("-r" "${P4ROOT}")
+CONFIGURE_P4D_CMD+=("-u" "${P4USER}")
+CONFIGURE_P4D_CMD+=("-P" "${P4PASSWD}")
+
+if [[ "${P4D_CASE_SENSITIVE}" == "true" ]]; then
+	CONFIGURE_P4D_CMD+=("--case" "0")
+else
+	CONFIGURE_P4D_CMD+=("--case" "1")
 fi
 
-# Delete default depot if required
-if [[ "${P4D_NO_DEPOT}" == "true" ]]; then
-    echo "Deleting default depot..."
-    p4 depot -d "depot" 1>/dev/null
-fi
-
-# Start the Service
-p4dctl start -t p4d "${P4SVCNAME}"
+"${CONFIGURE_P4D_CMD[@]}" 1>/dev/null
 
 # Automatically trust self if using ssl
 if echo "${P4PORT}" | grep -q '^ssl:'; then
 	p4 trust -y
 fi
 
+# Delete default depot if required
+if [[ "${P4_NO_DEFAULT_DEPOT}" == "true" ]]; then
+	echo "Deleting default depot..."
+	p4 depot -d "depot" 1>/dev/null
+fi
+
 # write details to p4 config
+#TODO don't want to do this..?
 cat > ~perforce/.p4config <<EOF
 P4USER=${P4USER}
 P4PORT=${P4PORT}
@@ -55,8 +61,8 @@ EOF
 
 # Setup Defaults
 if [ "${FRESHINSTALL}" = "1" ]; then
-    # Load up the default tables
-    echo >&2 "First time installation.. setting security configuration"
+	# Load up the default tables
+	echo >&2 "First time installation.. setting security configuration"
 	
 	p4 configure set security="${P4D_SECURITY}" 1>/dev/null		# Security Level
 	p4 configure set lbr.proxy.case=1							# disable automatic user account creation
@@ -67,9 +73,9 @@ fi
 # Output Properties
 echo "   P4USER=${P4USER} (the admin user)"
 
-if [ "${P4PASSWD}" == "pass12349ers!" ]; then
-    echo -e "\n***** WARNING: USING DEFAULT PASSWORD ******\n"
-    echo "Please change as soon as possible:"
-    echo "   P4PASSWD=$P4PASSWD"
-    echo -e "\n***** WARNING: USING DEFAULT PASSWORD ******\n"
+if [ "${P4PASSWD}" == "defaultPassword123" ]; then
+	echo -e "\n***** WARNING: USING DEFAULT PASSWORD ******\n"
+	echo "Please change as soon as possible:"
+	echo "   P4PASSWD=$P4PASSWD"
+	echo -e "\n***** WARNING: USING DEFAULT PASSWORD ******\n"
 fi
--- perforce-server/setup-perforce.sh
+++ perforce-server/setup-perforce.sh
@@ -7,26 +7,29 @@ export P4D_SECURITY="${P4D_SECURITY:-2}"
 export P4D_NO_DEFAULT_DEPOT="${P4D_NODEPOT:-true}"
 
 # link p4dctl service configuration file into /etc/perforce/
-CONFDIR="${DATADIR}/config"
-
-if [[ ! -d "${CONFDIR}"/etc ]]; then
+if [[ ! -d "${DATA_PATH}"/etc ]]; then
 	echo "Initializing configuration files in /etc/perforce/"
-	mkdir -p "${CONFDIR}"/etc
-	cp -rf /etc/perforce/* "${CONFDIR}"/etc
+	mkdir -p "${DATA_PATH}"/etc
+	cp -rf /etc/perforce/* "${DATA_PATH}"/etc
 	export FRESHINSTALL=1
 fi
 
 # setup hard link in docker volume directory to default perforce config location
 mv /etc/perforce /etc/perforce.orig
-ln -s "${CONFDIR}"/etc /etc/perforce
+ln -s "${DATA_PATH}"/etc /etc/perforce
 
 # Run all subscripts (in subshell to prevent environment variable changes)
+# This also allows these scripts to "exit" without breaking the flow
 (
 	for f in /docker-startup.d/*.sh; do
 		bash "${f}" || exit 1
 	done
 )
 
+# Not sure...
 sleep 2
 
-exec /usr/bin/tail --pid=$(cat /var/run/p4d.$P4SVCNAME.pid) -F "${DATADIR}/${P4SVCNAME}/logs/log"
+# Tail the log and output it
+exec /usr/bin/tail --pid=$(cat /var/run/p4d.$P4SVCNAME.pid) -F "${DATA_PATH}/${P4SVCNAME}/logs/log"
+
+exit 0
